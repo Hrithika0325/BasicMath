ba17b5a88aaf0532c8d341f0c90eb6aecadd66f5
Hrithika0325 <107783598+Hrithika0325@users.noreply.github.com>
2022-06-19 21:40:37 +0800

    Integer Operations
    Explore basic integer operations : add, subtract, multiplication and division

---
+++ Interger_operations.py
@@ -0,0 +1,18 @@
+print("Hello World!, This is my first program")
+print("integer operations with the numbers 144 and 12")
+
+a=144; b=12;
+sum = a+b
+print("sum :",  sum)
+
+diff = a-b
+print("diff :" , diff)
+
+product = a*b
+print("product:" , product)
+
+quotient = a/b
+print("quotient:" , quotient)
+
+remainder = a%b
+print("remainder:" , remainder)
