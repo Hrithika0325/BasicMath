7b33d056a7c20818da1c804a9bef14bed1832ff1
Hrithika0325 <107783598+Hrithika0325@users.noreply.github.com>
2022-06-19 21:52:42 +0800

    Basic String Operations (1)
    Explore around with Multiline string, Escape character, Extraction of characters and String concatenation

---
+++ String_operations1.py
@@ -0,0 +1,19 @@
+print("Hello World!, This is my fourth program")
+print("String operations")
+print("-" *45)
+print("Hrithika said \"How are you?\" ")
+print('''Multi-line string :
+msg one
+msg two
+msg three''')
+print("-" *45)
+print(" Extraction of string characters ")
+msg='Hello'
+print("first character : " , msg [0])
+print("fifth character : ", msg [4])
+print("-" *45)
+print(" String Concatenation ")
+msg1= "My name is "
+msg2= "Hrithika"
+msg3=msg1 + msg2
+print (msg3)
