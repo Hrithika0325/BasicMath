80db316f33e8b406b9b45c76da1fbd20a9460991
Hrithika0325 <107783598+Hrithika0325@users.noreply.github.com>
2022-06-19 21:49:25 +0800

    Bool Operations
    Play around with Bool Operations

---
+++ Bool_operations .py
@@ -0,0 +1,12 @@
+print("Hello World!, This is my third program")
+print("Bool operations with the numbers 4 and 5")
+
+a=4; b=5;
+x=a>b
+print("4>5 is" , x)
+
+y=a<b
+print("4<5 is" , y)
+
+z= a==b
+print("4==5 is", z)
