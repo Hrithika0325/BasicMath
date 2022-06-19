44f58fa7e11e9edd31c4649116322aefe773be1d
Hrithika0325 <107783598+Hrithika0325@users.noreply.github.com>
2022-06-19 21:42:46 +0800

    Float Operations
    Learn more about Float Operations ( Decimal Operations) - addition, subtraction, multiplication and division
    Enjoy!

---
+++ Float_operations.py
@@ -0,0 +1,18 @@
+print("Hello World!, This is my second program")
+print("Floating point operations with the numbers 20.1 and 2.5")
+
+a=20.1; b=2.5;
+sum = a+b
+print("sum :",  sum)
+
+diff = a-b
+print("diff :" , diff)
+
+product = a*b
+print("product:" , product)
+
+quotient = a/b
+print("quotient:" , quotient)
+
+remainder = a%b
+print("remainder:" , remainder)
