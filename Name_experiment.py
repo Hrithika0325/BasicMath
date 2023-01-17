68a1135701b8fae28765ec4df9653335e2f58582
Hrithika Kodihalli <107783598+Hrithika0325@users.noreply.github.com>
2023-01-17 19:52:49 +0800

    Name Experiment
    Try this on your computer! It is very fun and interesting at the same time!

---
+++ Name_experiment.py
@@ -0,0 +1,14 @@
+print("Name experiment")
+
+print("Ask for first name, second name. Full name is equal to first name plus second name. Print full name")
+
+firstname = input("Enter first name:")
+secondname = input("Enter second name:")
+
+fullname = firstname + ' ' + secondname
+
+print("Full name is" ,fullname)
+
+print("To reverse the name, this is how you do it:")
+# print reversed sorted list 
+print(fullname[::-1])
